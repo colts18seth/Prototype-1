7a10c25c9ab358749309b86802a83eefba838287
Seth <colts18seth@gmail.com>
2026-02-17 23:24:59 -0500

    finished optional challenges

--- Assets/Scripts/FollowPlayer.cs
+++ Assets/Scripts/FollowPlayer.cs
@@ -3,17 +3,11 @@ using UnityEngine;
 public class FollowPlayer : MonoBehaviour
 {
     public GameObject player;
-    public CameraController cameraController;
-    //private Vector3 offset = new Vector3(0, 5, -7);
+    private Vector3 offset = new Vector3(0, 5, -7);
 
     // Update is called once per frame
     void LateUpdate()
     {
-        //Offset the camera position to be above and behind the player
-        transform.position = player.transform.position + cameraController.offset;
-
-        if (cameraController.firstPersonCamera.enabled) {
-            transform.rotation = player.transform.rotation * Quaternion.Euler(18, 0, 0);
-        }
+        transform.position = player.transform.position + offset;
     }
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,15 +7,25 @@ public class PlayerController : MonoBehaviour
     private float horizontalInput;
     private float forwardInput;
 
+    public string inputID;
+    public Camera mainCamera;
+    public Camera hoodCamera;
+    public KeyCode switchKey;
+
     // Update is called once per frame
     void Update()
     {
-        horizontalInput = Input.GetAxis("Horizontal");
-        forwardInput = Input.GetAxis("Vertical");
+        horizontalInput = Input.GetAxis("Horizontal" + inputID);
+        forwardInput = Input.GetAxis("Vertical" + inputID);
 
         // Move the vehicle forward
         transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
         // Turn the vehicle
         transform.Rotate(Vector3.up * Time.deltaTime * turnSpeed * horizontalInput);
+
+        if(Input.GetKeyDown(switchKey)) { 
+            mainCamera.enabled = !mainCamera.enabled; 
+            hoodCamera.enabled = !hoodCamera.enabled; 
+        }
     }
 }
